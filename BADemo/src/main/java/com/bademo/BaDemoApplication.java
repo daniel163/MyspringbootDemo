3404d63280005599f2944d8ba0a8a0015e4702f3
nickzhang <nickzhang@tcl.com>
2016-11-29 09:29:57 +0800

    update

---
+++ BADemo/src/main/java/com/bademo/BaDemoApplication.java
@@ -0,0 +1,12 @@
+package com.bademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaDemoApplication.class, args);
+	}
+}
---
+++ BADemo/src/test/java/com/bademo/BaDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bademo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BaDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
